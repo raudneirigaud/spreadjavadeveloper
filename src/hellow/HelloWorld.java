660da12e3899be7a189c93e771a6c971383d405b
Raudnei Rigaud <raudneiweb@gmail.com>
2021-12-05 01:58:18 -0300

    repositorio java dio

---
+++ src/hellow/HelloWorld.java
@@ -0,0 +1,17 @@
+package hellow;
+
+public class HelloWorld {
+	
+	
+	static String nome;
+	
+	public static void main(String[] args) {
+		
+		nome = "Raudnei";
+		
+		
+		System.out.println("Olá, mundo! Me chamo "+ nome);
+		
+	}
+
+}
